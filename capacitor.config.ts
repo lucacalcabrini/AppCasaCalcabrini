8237c3c9f7d318950dbfc56b7a189a8c342534df
lucacalcabrini <luca.calcabrini@hotmail.com>
2026-05-08 22:51:04 +0200

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,8 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+const config: CapacitorConfig = {
+  appId: 'it.calcabrini.casa',
+  appName: 'Casa',
+  webDir: 'dist',
+  server: { androidScheme: 'https' }
+};
+export default config;
