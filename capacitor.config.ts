433608fa9598c796e186fd994bfc4cc85018d7ad
lucacalcabrini <luca.calcabrini@hotmail.com>
2026-05-09 08:52:03 +0200

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,8 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-const config: CapacitorConfig = {
-  appId: 'it.calcabrini.casa',
-  appName: 'Casa',
-  webDir: 'dist',
-  server: { androidScheme: 'https' }
-};
-export default config;
